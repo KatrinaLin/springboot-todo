23a75cfe8e5435cf24780a0d1c55272a38fe5a91
Celin <shyli@thoughtworks.com>
2018-09-14 18:07:27 +0800

    modify PostMapping

--- src/main/java/com/example/demo/controller/ToDoController.java
+++ src/main/java/com/example/demo/controller/ToDoController.java
@@ -16,21 +16,28 @@ public class ToDoController {
     }
 
     @PostMapping("/todos")
-    public String addToDo(@RequestParam int id, @RequestParam String value, @RequestParam boolean checked) {
+    public ArrayList<Item> addToDo(@RequestParam String value, @RequestParam boolean checked) {
+        int size = this.toDoList.size();
+        int id;
+        if (size == 0) {
+            id = 1;
+        } else {
+            id = this.toDoList.get(size - 1).getId() + 1;
+        }
         this.toDoList.add(new Item(id, value, checked));
-        return "Success!";
+        return this.toDoList;
     }
 
 
-    @DeleteMapping("/todos")
-    public String deleteToDo(@RequestParam int id) {
+    @DeleteMapping("/todos/{id:\\d+}")
+    public String deleteToDo(@PathVariable("id") int id) {
         Item target = this.toDoList.stream().filter(item -> item.getId() == id).findFirst().get();
         this.toDoList.remove(target);
         return "Success!";
     }
 
-    @PutMapping("/todos")
-    public String updateToDo(@RequestParam int id, @RequestParam String newValue){
+    @PutMapping("/todos/{id:\\d+}")
+    public String updateToDo(@PathVariable("id") int id, @RequestParam String newValue) {
         Item target = this.toDoList.stream().filter(item -> item.getId() == id).findFirst().get();
         target.setValue(newValue);
         return "Success!";
